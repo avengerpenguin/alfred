a1f822cb55ecc15ea0098430830ff25fb07b3165
AvengerPenguin <ross.fenning@gmail.com>
2015-02-24 09:36:15 +0000

    Added top command that gets my top 5 todo items.

--- plugins/trello.py
+++ plugins/trello.py
@@ -10,7 +10,7 @@ class Trello(BotPlugin):
     def __init__(self):
         #self.conn = TrelloConnection('3dd167f33e6de7d4f13fcd138346210c', 'ba57d7a637f46d664b8e30a0d8f50642c55c117dadf23cf7c699c51be5e36cbd')
         self.client = Client(api_key=os.getenv('TRELLO_APP_KEY'), user_auth_token=os.getenv('TRELLO_OAUTH_TOKEN'))
-        self.board = Board(self.client, 'ahbcCkvQ')
+        self.board = Board(self.client, os.getenv('TRELLO_BOARD'))
 
     def kanban_walker(self):
         flow = reversed(self.board.get_lists())
